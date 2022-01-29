e9c7a5221caf2a8005d628ebab2aa5b1b8fa35fe
Jannify <23176718+Jannify@users.noreply.github.com>
2022-01-29 03:21:04 +0100

    Changed Author

--- AssetRipperCommon/BuildInfo.cs
+++ AssetRipperCommon/BuildInfo.cs
@@ -9,14 +9,14 @@
 		public const string GUIName = Name + "GUI";
 		public const string LibraryName = Name + "Library";
 
-		public const string Author = "ds5678";
+		public const string Author = "ds5678 edited by Jannify";
 		public const string CopyRight = "Copyright © 2022";
 
 		public const string Version = "0.1.9.3";
 
 		public const string TranslationURL = "https://weblate.samboy.dev/engage/assetripper/";
 		public const string WebsiteURL = "https://assetripper.github.io/AssetRipper";
-		public const string RepositoryURL = "https://www.github.com/AssetRipper/AssetRipper";
+		public const string RepositoryURL = "https://www.github.com/Jannify/AssetRipper";
 		public const string IssuesURL = RepositoryURL + "/issues";
 		public const string PullRequestsURL = RepositoryURL + "/pulls";
 		public const string ReleasesURL = RepositoryURL + "/releases";
